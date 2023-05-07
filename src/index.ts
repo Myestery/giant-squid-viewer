314f876d792a63cd475431a265f4aabb89aaeb2e
Johnpaul <polymenjohn1@gmail.com>
2023-05-07 23:50:18 +0100

    update exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@
 import { GSCoin, GSCoins, GSCoinsMap } from './types/coins';
 
 import Viewer from "./components/viewer";
-import Coincard from "./components/coincard";
+import CoinCard from "./components/coincard";
 
-export { Viewer,GSCoin, GSCoins, GSCoinsMap,Coincard };
+export { Viewer,GSCoin, GSCoins, GSCoinsMap,CoinCard };
